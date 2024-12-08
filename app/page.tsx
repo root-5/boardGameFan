61f2415b5726c0685c6f79cb939556b4701be40a
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2024-12-09 06:13:55 +0900

    動作確認済み、グループ関数を別途用意して整形

--- app/page.tsx
+++ app/page.tsx
@@ -11,44 +11,49 @@ const isLocal = true;
 const modelPath = 'Dice.glb';
 const backgroundImagePath = isLocal ? 'bg_.jpg' : 'bg.jpg';
 
-// ダイスモデル
+// ダイスモデル単体
 function DiceModel() {
   const { scene } = useGLTF(modelPath)
-  // const [rotation, setRotation] = useState([0, 0, 0])
-
-  //モデルの中心を再設定
-  // useEffect(() => {
-  //   scene.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 0, 0));
-  //   // scene.makeTranslation
-  // }, [scene]);
-
-  // マイフレームの処理
-  // useFrame(() => {
-  //   setRotation([
-  //     rotation[0] + Math.PI * 1 / 100,
-  //     rotation[1] + Math.PI * 1 / 100,
-  //     rotation[2] + Math.PI * 1 / 100
-  //   ])
-  // })
 
   return <primitive
     object={scene}
+    // 今回はサイコロの中心が原点になるように調整、元は中心が底面になっている
     position={[0, -5, 0]} // モデルの位置
     scale={[100,100,100]} // モデルの大きさ
-    // rotation={rotation} // モデルの回転
-    // translate={[0, 0, 0]} // モデルの中心を再設定
-    // onClick={() => { // クリック時の処理
-    //   setRotation([
-    //     rotation[0] + Math.PI * 1 / 100,
-    //     rotation[1] + Math.PI * 0 / 100,
-    //     rotation[2] + Math.PI * 0 / 100
-    //   ])
-    // }}
   />
 }
 
+// グループ（複数のモデルの集合）
+function Group() {
+  const [rotation, setRotation] = useState([0, 0, 0]);
+
+  useFrame(() => {
+    // モデルの回転
+    setRotation([
+      rotation[0] + Math.PI * 1 / 100,
+      rotation[1] + Math.PI * 1 / 100,
+      rotation[2] + Math.PI * 1 / 100
+    ])
+  })
+
+  return <group
+    // position={[0, -5, 0]} // モデルの位置
+    rotation={rotation} // モデルの回転
+    onClick={() => { // クリック時の処理
+        console.log('click');
+        setRotation([
+          rotation[0] + Math.PI * 1 / 100,
+          rotation[1] + Math.PI * 0 / 100,
+          rotation[2] + Math.PI * 0 / 100
+        ])
+      }}
+    >
+    {/* モデル */}
+    <DiceModel />
+  </group>
+}
+
 export default function App() {
-  const [rotation, setRotation] = useState([0, 0, 0])
 
   return (
     <>
@@ -60,28 +65,15 @@ export default function App() {
         }}
       >
 
-        {/* モデル */}
-        <group
-          // position={[0, -5, 0]} // モデルの位置
-          rotation={rotation} // モデルの回転
-          onClick={() => { // クリック時の処理
-            console.log('click');
-            setRotation([
-              rotation[0] + Math.PI * 1 / 100,
-              rotation[1] + Math.PI * 0 / 100,
-              rotation[2] + Math.PI * 0 / 100
-            ])
-          }}
-        >
-          <DiceModel />
-        </group>
+        {/* グループ */}
+        <Group />
 
         {/* カメラ操作 https://threejs.org/docs/#examples/en/controls/OrbitControls */}
         <OrbitControls
           minDistance={20} // ズームの最小距離
           maxDistance={100} // ズームの最大距離
-          // minPolarAngle={Math.PI * 43.5 / 100} // カメラの上下回転角度の最小値
-          // maxPolarAngle={Math.PI * 59 / 100} // カメラの上下回転角度の最大値
+          minPolarAngle={Math.PI * 43.5 / 100} // カメラの上下回転角度の最小値
+          maxPolarAngle={Math.PI * 59 / 100} // カメラの上下回転角度の最大値
           // autoRotate // 自動回転
         />
 
