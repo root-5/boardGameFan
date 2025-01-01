7cf3ea157c72541ea11d20b14967ba4ef278694c
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-01-02 04:55:38 +0900

    色設定コンポーネントを別関数に抜き出し

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,59 @@ const initialComponents = [
   { component: "roulette" },
 ];
 
+const StyleSettingCard = ({
+  bgColor_1,
+  setBgColor_1,
+  bgColor_2,
+  setBgColor_2,
+  fontColor,
+  setFontColor,
+}: {
+  bgColor_1: string;
+  setBgColor_1: (color: string) => void;
+  bgColor_2: string;
+  setBgColor_2: (color: string) => void;
+  fontColor: string;
+  setFontColor: (color: string) => void;
+}) => {
+  return (
+    <div
+      className={"relative w-56 h-56 text-center"}
+      style={{ backgroundColor: bgColor_1, color: fontColor }}
+    >
+      <div className="py-10 px-5 flex flex-col gap-4 justify-center items-center">
+        <div className="grid grid-cols-2 gap-4 place-content-between">
+          <div className="block w-28 text-left">{"Color 1"}: </div>
+          <input
+            className="block w-16"
+            type="color"
+            value={bgColor_1}
+            onChange={(e) => setBgColor_1(e.target.value)}
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-4 place-content-between">
+          <div className="block w-28 text-left">{"Color 2"}: </div>
+          <input
+            className="block w-16"
+            type="color"
+            value={bgColor_2}
+            onChange={(e) => setBgColor_2(e.target.value)}
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-4 place-content-between">
+          <div className="block w-28 text-left">{"Font Color"}: </div>
+          <input
+            className="block w-16"
+            type="color"
+            value={fontColor}
+            onChange={(e) => setFontColor(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function App() {
   const [componentList, setComponentList] = useState(initialComponents);
   const [bgColor_1, setBgColor_1] = useState("#222233");
@@ -68,51 +121,25 @@ export default function App() {
     <>
       <div className={"flex flex-wrap"} style={{ zoom: 1 }} id="card-container">
         {/* スタイル設定用カード */}
-        <div
-          className={"relative w-56 h-56 text-center"}
-          style={{ backgroundColor: bgColor_1, color: fontColor }}
-        >
-          <div className="py-10 px-5 flex flex-col gap-4 justify-center items-center">
-            <div className="grid grid-cols-2 gap-4 place-content-between">
-              <div className="block w-28 text-left">{"Color 1"}: </div>
-              <input
-                className="block w-16"
-                type="color"
-                value={bgColor_1}
-                onChange={(e) => setBgColor_1(e.target.value)}
-              />
-            </div>
-            <div className="grid grid-cols-2 gap-4 place-content-between">
-              <div className="block w-28 text-left">{"Color 2"}: </div>
-              <input
-                className="block w-16"
-                type="color"
-                value={bgColor_2}
-                onChange={(e) => setBgColor_2(e.target.value)}
-              />
-            </div>
-            <div className="grid grid-cols-2 gap-4 place-content-between">
-              <div className="block w-28 text-left">{"Font Color"}: </div>
-              <input
-                className="block w-16"
-                type="color"
-                value={fontColor}
-                onChange={(e) => setFontColor(e.target.value)}
-              />
-            </div>
-          </div>
-        </div>
+        <StyleSettingCard
+          bgColor_1={bgColor_1}
+          setBgColor_1={setBgColor_1}
+          bgColor_2={bgColor_2}
+          setBgColor_2={setBgColor_2}
+          fontColor={fontColor}
+          setFontColor={setFontColor}
+        />
         {componentList.map((item, index) => {
           const itemBgColor = index % 2 !== 0 ? bgColor_1 : bgColor_2;
           // Setter の場合は Setter コンポーネントを表示
           if (item.component === "setter") {
             return (
               <div
+                key={index}
                 className={"relative w-56 h-56 text-center"}
                 style={{ backgroundColor: itemBgColor, color: fontColor }}
               >
                 <Setter
-                  key={index}
                   componentMap={componentMap}
                   componentList={componentList}
                   setComponentList={setComponentList}
@@ -122,21 +149,18 @@ export default function App() {
           }
           // 指定のコンポーネントを取得
           const Component = componentMap[item.component];
-          return (function () {
-            const itemFontColor = fontColor;
-            return (
-              <div
-                key={index}
-                className={"relative w-56 h-56 text-center"}
-                style={{
-                  backgroundColor: itemBgColor,
-                  color: itemFontColor,
-                }}
-              >
-                <Component />
-              </div>
-            );
-          })();
+          return (
+            <div
+              key={index}
+              className={"relative w-56 h-56 text-center"}
+              style={{
+                backgroundColor: itemBgColor,
+                color: fontColor,
+              }}
+            >
+              <Component />
+            </div>
+          );
         })}
       </div>
     </>
