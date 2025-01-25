1e0ce97157db6c314728295466877e0c1eee228b
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-01-26 01:02:10 +0900

    StyleSetting のリファクタ

--- components/card/StyleSetting.tsx
+++ components/card/StyleSetting.tsx
@@ -1,5 +1,26 @@
+'use Client';
+
 import { CardStyle } from "../../utils/types";
 import { initialStyle } from "../../utils/cardDefinitions";
+import { setLocalStorage } from "../../utils/localStorageUtils";
+
+const colorFields = [
+  { label: "Base 1", key: "bgColor_1" },
+  { label: "Font 1", key: "fontColor_1" },
+  { label: "Base 2", key: "bgColor_2" },
+  { label: "Font 2", key: "fontColor_2" },
+];
+
+const fontOptions = [
+  "Comic Sans MS",
+  "Arial",
+  "Courier",
+  "Georgia",
+  "Impact",
+  "Times New Roman",
+  "Trebuchet MS",
+  "Verdana",
+];
 
 export default function StyleSetting({
   cardStyle,
@@ -10,45 +31,20 @@ export default function StyleSetting({
 }) {
   return (
     <div className={"relative w-56 h-56 text-center"}>
-      <div className="py-6 flex flex-col justify-center items-center gap-3">
-        <div className="px-8 grid grid-cols-2 gap-x-2 justify-center items-center">
-          <div className="pt-0.5 block text-left">{"Base 1"}: </div>
-          <input
-            className="block w-6 m-auto bg-transparent cursor-pointer"
-            type="color"
-            value={cardStyle.bgColor_1}
-            onChange={(e) =>
-              setCardStyle({ ...cardStyle, bgColor_1: e.target.value })
-            }
-          />
-          <div className="h-6 pt-0.5 block text-left">{"Font 1"}: </div>
-          <input
-            className="block w-6 m-auto bg-transparent cursor-pointer"
-            type="color"
-            value={cardStyle.fontColor_1}
-            onChange={(e) =>
-              setCardStyle({ ...cardStyle, fontColor_1: e.target.value })
-            }
-          />
-          <div className="h-6 pt-0.5 block text-left">{"Base 2"}: </div>
-          <input
-            className="block w-6 m-auto bg-transparent cursor-pointer"
-            type="color"
-            value={cardStyle.bgColor_2}
-            onChange={(e) =>
-              setCardStyle({ ...cardStyle, bgColor_2: e.target.value })
-            }
-          />
-          <div className="h-6 pt-0.5 block text-left">{"Font 2"}: </div>
-          <input
-            className="block w-6 m-auto bg-transparent cursor-pointer"
-            type="color"
-            value={cardStyle.fontColor_2}
-            onChange={(e) =>
-              setCardStyle({ ...cardStyle, fontColor_2: e.target.value })
-            }
-          />
-        </div>
+      <div className="py-6 flex flex-col justify-center items-center gap-2">
+        {colorFields.map((field) => (
+          <div key={field.key} className="px-8 grid grid-cols-2 gap-x-2 justify-center items-center">
+            <div className="block text-left">{field.label}: </div>
+            <input
+              className="block w-6 m-auto bg-transparent cursor-pointer"
+              type="color"
+              value={cardStyle[field.key as keyof CardStyle]}
+              onChange={(e) =>
+                setCardStyle({ ...cardStyle, [field.key]: e.target.value })
+              }
+            />
+          </div>
+        ))}
         <select
           className="block w-40 bg-transparent cursor-pointer"
           value={cardStyle.fontStyle}
@@ -56,36 +52,18 @@ export default function StyleSetting({
             setCardStyle({ ...cardStyle, fontStyle: e.target.value })
           }
         >
-          <option className="bg-transparent text-black" value="Comic Sans MS">
-            Comic Sans MS
-          </option>
-          <option className="bg-transparent text-black" value="Arial">
-            Arial
-          </option>
-          <option className="bg-transparent text-black" value="Courier">
-            Courier
-          </option>
-          <option className="bg-transparent text-black" value="Georgia">
-            Georgia
-          </option>
-          <option className="bg-transparent text-black" value="Impact">
-            Impact
-          </option>
-          <option className="bg-transparent text-black" value="Times New Roman">
-            Times New Roman
-          </option>
-          <option className="bg-transparent text-black" value="Trebuchet MS">
-            Trebuchet MS
-          </option>
-          <option className="bg-transparent text-black" value="Verdana">
-            Verdana
-          </option>
+          {fontOptions.map((font) => (
+            <option key={font} className="bg-transparent text-black" value={font}>
+              {font}
+            </option>
+          ))}
         </select>
       </div>
       <div
         className="absolute bottom-1 left-2 text-xl cursor-pointer duration-200 opacity-30 hover:opacity-100"
         onClick={() => {
           setCardStyle(initialStyle);
+          setLocalStorage("cardStyle", JSON.stringify(initialStyle));
         }}
       >
         ↺
