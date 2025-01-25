43b5faa26b8151c1200fc74d2d65778c3be2d253
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-01-26 01:41:08 +0900

    ホットリロードにディレイを追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 const nextConfig = {
   /* config options here */
   output: "export",
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        aggregateTimeout: 1000, // 300ms の遅延を追加
+        poll: 1000, // 1秒ごとにファイルの変更をチェック
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
