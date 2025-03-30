1ef4a5bb22970b3855de8e2ef28ff64d6817746c
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-03-31 05:36:19 +0900

    ローカル用設定戻し

--- utils/cardDefinitions.ts
+++ utils/cardDefinitions.ts
@@ -4,17 +4,17 @@
  */
 
 import Dice from "../components/card/Dice";
-// import Coin from "../components/card/Coin";
-// import Roulette from "../components/card/Roulette";
-// import OneOnOne from "../components/card/OneOnOne";
-// import Timer from "../components/card/Timer";
-// import Token from "../components/card/Token";
-// import Score from "../components/card/Score";
-// import Turn from "../components/card/Turn";
-// import Winner from "../components/card/Winner";
-// import PlayerSetting from "../components/card/PlayerSetting";
-// import StyleSetting from "../components/card/StyleSetting";
-// import Info from "../components/card/Info";
+import Coin from "../components/card/Coin";
+import Roulette from "../components/card/Roulette";
+import OneOnOne from "../components/card/OneOnOne";
+import Timer from "../components/card/Timer";
+import Token from "../components/card/Token";
+import Score from "../components/card/Score";
+import Turn from "../components/card/Turn";
+import Winner from "../components/card/Winner";
+import PlayerSetting from "../components/card/PlayerSetting";
+import StyleSetting from "../components/card/StyleSetting";
+import Info from "../components/card/Info";
 import { CardComponent } from "./types";
 import { Shantell_Sans, Sansita_Swashed, Dancing_Script, Libre_Bodoni, Pixelify_Sans, Red_Hat_Text, Caveat, Stick_No_Bills, Tilt_Prism, Antonio } from 'next/font/google'
 
@@ -23,17 +23,17 @@ export const cardMap: {
   [key: string]: CardComponent;
 } = {
   dice: Dice,
-  // coin: Coin,
-  // roulette: Roulette,
-  // oneOnOne: OneOnOne,
-  // timer: Timer,
-  // token: Token,
-  // score: Score,
-  // turn: Turn,
-  // winner: Winner,
-  // playerSetting: PlayerSetting,
-  // styleSetting: StyleSetting,
-  // info: Info,
+  coin: Coin,
+  roulette: Roulette,
+  oneOnOne: OneOnOne,
+  timer: Timer,
+  token: Token,
+  score: Score,
+  turn: Turn,
+  winner: Winner,
+  playerSetting: PlayerSetting,
+  styleSetting: StyleSetting,
+  info: Info,
 
   // setter は特殊なコンポーネントなので、ここでは設定しない
   // setter: Setter,
@@ -46,17 +46,17 @@ export const maxRange = { rows: 20, cols: 20 };
 // PC の場合は x,y で画面を構成、SP の場合は配列の順番で画面を構成
 export const initialCardsList = [
   { component: "dice", x: 0, y: 1 },
-  // { component: "coin", x: 2, y: 0 },
-  // { component: "roulette", x: 2, y: 1 },
-  // { component: "oneOnOne", x: 2, y: 2 },
-  // { component: "timer", x: 0, y: 2 },
-  // { component: "token", x: 1, y: 1 },
-  // { component: "score", x: 1, y: 2 },
-  // { component: "turn", x: 1, y: 0 },
-  // { component: "winner", x: 0, y: 0 },
-  // { component: "playerSetting", x: 0, y: 3 },
-  // { component: "styleSetting", x: 1, y: 3 },
-  // { component: "info", x: 2, y: 3 },
+  { component: "coin", x: 2, y: 0 },
+  { component: "roulette", x: 2, y: 1 },
+  { component: "oneOnOne", x: 2, y: 2 },
+  { component: "timer", x: 0, y: 2 },
+  { component: "token", x: 1, y: 1 },
+  { component: "score", x: 1, y: 2 },
+  { component: "turn", x: 1, y: 0 },
+  { component: "winner", x: 0, y: 0 },
+  { component: "playerSetting", x: 0, y: 3 },
+  { component: "styleSetting", x: 1, y: 3 },
+  { component: "info", x: 2, y: 3 },
 ];
 
 // 初期スタイル
